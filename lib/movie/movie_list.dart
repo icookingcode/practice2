3821e4f9f1883f52479b4b58527b0383c9f1c251
guchaochao <guchaochao@huiyunit.com>
2019-06-04 09:17:10 +0800

    使用dio获取网络数据，绘制item

--- lib/movie/movie_list.dart
+++ lib/movie/movie_list.dart
@@ -1,15 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
 class MovieList extends StatefulWidget {
   ///in_theaters :正在热映  coming_soon:即将上映 top250 ： Top250
   final String type;
-  MovieList({Key key,@required this.type}):super(key:key);
+
+  MovieList({Key key, @required this.type}) : super(key: key);
+
   @override
   _MovieListState createState() => _MovieListState();
 }
 
 class _MovieListState extends State<MovieList> {
+  Dio _dio;
+  int page = 1;
+  int pageSize = 10;
+  var mList = [];
+  int total = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _dio = Dio();
+    getMovieList();
+  }
+
+  Widget _movieItem(BuildContext context, int index) {
+    var item = mList[index];
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Image.network(
+              item['images']['small'],
+              height: 180,
+              width: 130,
+              fit: BoxFit.cover,
+            ),
+            Container(
+              height: 200,
+              padding: EdgeInsets.only(left: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Text('电影名称：${item['title']}'),
+                  Text('上映年份：${item['year']}年'),
+                  Text('电影类型：${item['genres'].join('.')}'),
+                  Text('豆瓣评分：${item['rating']['average']}分'),
+                  Text('主要演员：${item['title']}'),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Divider(height: 2,color: Colors.grey,),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('这是电影列表 + ${widget.type}');
+//    return Text('这是电影列表 + ${widget.type} + ${mList.length}/$total');
+    return ListView.builder(
+      itemBuilder: _movieItem,
+      itemCount: mList.length,
+    );
+  }
+
+  ///http://www.liulongbin.top:3005/api/v2/movie/${widget.type}?start=偏移量&count=每页显示多少条数据
+  getMovieList() async {
+    var resoponse = await _dio.get(
+        'http://www.liulongbin.top:3005/api/v2/movie/${widget.type}?start=${(page - 1) * pageSize}&count=$pageSize');
+    var result = resoponse.data;
+    setState(() {
+      //通过dio返回的数据必须用中括号引用
+      mList = result['subjects'];
+      total = result['total'];
+    });
   }
 }
